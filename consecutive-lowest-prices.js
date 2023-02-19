5e5ecd9ff9de63b7ce61b1751f62a3b8d37d019a
Thomas Edvardsen <ThomasEdvardsen@users.noreply.github.com>
2023-02-20 00:33:36 +0100

    Refactoring

--- consecutive-lowest-prices.js
+++ consecutive-lowest-prices.js
@@ -1,63 +1,42 @@
-//This script finds consecutive lowest prices
+// Find consecutive lowest prices
 
-//Argument should be a JSON object:
-//{
-//  "remaining_hours": 3, 
-//  "finished_hour": 8, 
-//  "prices": POWER_PRICES_JSON
-//}
-const arguments = JSON.parse(args[0]);
-const hours = parseInt(arguments.remaining_hours);
+// This script finds the start time for a period with a specified 
+// number of consecutive hours, with the lowest total price.
 
-//typically at 08:00 -> 8
-const finished_hour = parseInt(arguments.finished_hour);
-
-// Price data fetched from Heating Controller
+// Argument should be a JSON object:
+//
+// {
+//   "remaining_hours": 3, 
+//   "finished_hour": 8, 
+//   "prices": <POWER_PRICES_JSON>
+// }
+//
+// Where <POWER_PRICES_JSON> is the price data fetched from Heating Controller
 // Be aware of that the timestamps is in UTC time
 //  [
 //    {"startsAt":"2023-01-23T23:00:00.000Z","time":1674514800,"price":0.56033},
 //    {"startsAt":"2023-01-24T00:00:00.000Z","time":1674518400,"price":0.5583},
 //    {"startsAt":"2023-01-24T01:00:00.000Z","time":1674522000,"price":0.55841}
 //  ]
-const priceData = arguments.prices;
 
-function findLowestPriceSeries(data, hours) {
-  let lowestSum = Number.MAX_VALUE;
-  let lowestIndex = -1;
+const json = JSON.parse(args[0]);
+const remaining_hours = parseInt(json.remaining_hours);
+const finished_hour = parseInt(json.finished_hour);
+const prices = json.prices;
 
-  // Iterate over the data array and find the lowest sum of prices
-  for (let i = 0; i < data.length - hours + 1; i++) {
-    let sum = 0;
-
-    for (let j = i; j < i + hours; j++) {
-      sum += data[j].price;
-    }
-
-    if (sum < lowestSum) {
-      lowestSum = sum;
-      lowestIndex = i;
-    }
-  }
-
-  // Return the epoch for the first object in the series
-  if (lowestIndex >= 0) {
-    return data[lowestIndex].time;
-  }
-
-  return -1;
+function convertDate(dateIn, offset) {
+  return new Date(dateIn.getTime() + (offset * 60 * 60 * 1000));
 }
 
-
-function getUTCDate(date, offset) {
-  return new Date(date.getTime() - (offset * 60 * 60 * 1000));
+function epochToDate(epochInSec, offset) {
+  const date = new Date(epochInSec*1000);
+  return convertDate(date, offset);
 }
 
+function convertDateToEpochInSec(date) {
+  return Math.round(date.getTime() / 1000);
+}
 
-// Norwegian standard time (CET) is one hour ahead of universal time (UTC). 
-// Transition to Central European Summer Time (CEST) is done by setting the clock forward one hour, 
-// and then Norwegian time is two hours ahead of universal time (UTC). 
-// It will take place at 02.00 last Sunday in March. 
-// Return to normal time takes place at 03.00 on the last Sunday in October.
 function getTimeZoneOffsetForNorway() {
   const now = new Date();
   const month = now.getMonth() + 1; // 0-based index, so we add 1
@@ -81,45 +60,79 @@ function getTimeZoneOffsetForNorway() {
   }
 }
 
+function getUtcEndDate(endHour, offset) {
+  // create a date object with the given hour
+  let endDate = new Date();
+  //increasing the date by one day if the hour has already passed on the current date
+  if (endDate.getHours() >= endHour) {
+    endDate.setDate(endDate.getDate() + 1);
+  }
+  endDate.setHours(endHour);
+  endDate.setMinutes(0,0,0);  
+
+  return convertDate(endDate, offset)
+}
+
+
+function findLowestPriceSeries(data, numberOfHours) {
+  let lowestSum = Number.MAX_VALUE;
+  let lowestIndex = -1;
+
+  // Iterate over the data array and find the lowest sum of prices
+  for (let i = 0; i < data.length - numberOfHours + 1; i++) {
+    let sum = 0;
+
+    for (let j = i; j < i + numberOfHours; j++) {
+      sum += data[j].price;
+    }
+
+    if (sum < lowestSum) {
+      lowestSum = sum;
+      lowestIndex = i;
+    }
+  }
+
+  if (lowestIndex >= 0) {
+    // Return the epoch for the first object in the series
+    return data[lowestIndex].time;
+  } else {
+    console.log('Could not find interval. Defaulting to now.')
+    // Get the epoch of the current hour
+    const currentHour = new Date();
+    currentHour.setMinutes(0, 0, 0);
+    return convertDateToEpochInSec(currentHour);
+  }
+}
+
+
+
 //
 // MAIN
 //
+const timeZoneOffset = getTimeZoneOffsetForNorway();
+console.log('timeZoneOffset: ', timeZoneOffset);
 
-const offset = getTimeZoneOffsetForNorway();
-
-// Define startdate to this hour
+// Define startdate in UTC format, to this hour: ie. 2023-02-19T21:51:51.305 --> 2023-02-19T21:00:00.000
 const utcStartDate = new Date();
-utcStartDate.setMinutes(0);
-utcStartDate.setSeconds(0);
-utcStartDate.setMilliseconds(0);
+utcStartDate.setMinutes(0,0,0);
 
-// Define end time
-let endDate = new Date();
-if (endDate.getHours() >= finished_hour) {
-  endDate.setDate(endDate.getDate() + 1);
-}
-endDate.setHours(finished_hour);
-endDate.setMinutes(0);
-endDate.setSeconds(0);
-endDate.setMilliseconds(0);
+// Define end time in UTC format
+const utcEndDate = getUtcEndDate(finished_hour, -timeZoneOffset);
 
-const utcEndDate = getUTCDate(endDate, offset);
-
-// filter ut prices for the given date interval
-const filteredData = priceData.filter(item => {
-  //convert epoch time to date
-  const givenDate = new Date(item.time*1000);
-  return givenDate >= utcStartDate && givenDate < utcEndDate;
+// filter ut prices for the date interval based by the given start and stop dates
+const filteredData = prices.filter(item => {
+  const itemDate = new Date(item.startsAt);
+  return itemDate >= utcStartDate && itemDate < utcEndDate;
 });
+//console.log('filteredData: ', filteredData);
 
-console.log('filteredData: ', filteredData);
+// find the start time of the cheapes date interval
+const startEpoch = findLowestPriceSeries(filteredData, remaining_hours);
+console.log('startEpoch: ', startEpoch);
 
-// find the interval
-const epochStart= findLowestPriceSeries(filteredData, hours);
+const startDate = epochToDate(startEpoch, timeZoneOffset);
+console.log("startDate: ", startDate.toLocaleString("no-NO"));
 
-const startDate =  getUTCDate(new Date(epochStart*1000), offset);
-console.log("start: ", startDate.toLocaleString("no-NO"));
-
-await tag('CheapestPricesStart',  startDate.toLocaleString("no-NO"))
+await tag('CheapestPricesStart',  startDate.toLocaleString("no-NO"));
 
 return true;
